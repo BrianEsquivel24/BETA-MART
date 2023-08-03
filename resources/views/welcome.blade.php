1842ef3b3654ddf3a95ca26a7b689ce2399b64d9
Brian Esquivel <19151684@aguascalientes.tecnm.mx>
2023-08-02 21:24:24 -0600

    public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="TextoQueCambie" content="width=device-width, initial-scale=1">
 
-        <title>Thai apestosa me encantas</title>
+        <title>Thai apestosa me eencantas</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
